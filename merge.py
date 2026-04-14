3673adec172cb72e194934a7fb5f584ebf323d41
saeosanm-ctrl <sae.osanm@gmail.com>
2026-04-15 02:06:10 +0900

    Update merge.py

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # =========================
 # データ取得日（今日）
 # =========================
-today = datetime.utcnow()+timedelta(hours=9).strftime("%Y%m%d-%H%M")
+today = datetime.utcnow()+timedelta(hours=9).strftime("%Y%m%d_%H%M")
 
 # =========================
 # CSV取得
